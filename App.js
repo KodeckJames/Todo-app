5532c14c45bb69723c9faf9004002e1d4f05e57c
Kodeck James <kodeckjames095@gmail.com>
2024-05-29 00:19:19 +0300

    second commit

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState('James')
+  const [person, setPerson] = useState({name: 'Mario', age: 40})
+
+  const clickHandler = () => {
+    setName('Kwamzy')
+    setPerson({name: 'Luigi', age: 50})
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My name is {name}</Text>
+      <Text>My name is {person.name} and I am {person.age} Years old</Text>
+      <View style={styles.buttonContainer}><Button title='update state' onPress={clickHandler}/></View>
       <StatusBar style="auto" />
+      
     </View>
   );
 }
@@ -16,5 +29,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  },  
+  buttonContainer: {
+    marginTop: 20
+  }
 });
